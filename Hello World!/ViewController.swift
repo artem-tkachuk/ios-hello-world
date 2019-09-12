655229723a6eb009a39c081c387fc5a81503365d
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-09-11 19:42:55 -0700

    Initial Commit

---
+++ Hello World!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World!
+//
+//  Created by Artem Tkachuk on 11/09/2019.
+//  Copyright © 2019 Uttpic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
